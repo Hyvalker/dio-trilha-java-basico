af6a838396142f6650411505a3f1e9ea1a7e8e73
Hyvalker <hyva.allan@gmail.com>
2025-03-23 19:21:17 -0300

    Pede para o usuário preencher informação a respeito dos dados bancários e imprime uma mensagem usando os dados preenchidos pelo usuário.

---
+++ ContaBanco/src/contabanco/ContaTerminal.java
@@ -0,0 +1,29 @@
+package contabanco;
+import java.util.Scanner;
+public class ContaTerminal {
+public static void main(String[] args) {
+	//TODO: Conhecer e importar a classe Scanner
+	
+	//Exibir as mensagens para o usuário
+	
+	//Obter pela Scanner os valores digitados no terminal
+	
+	//Exibir a mensagem final
+	
+	Scanner scanner = new Scanner (System.in);
+	System.out.println("Digite seu nome: ");
+	String nome = scanner.nextLine();
+	
+	System.out.println("Digite o número da agência");
+	String agencia = scanner.nextLine();
+	
+	System.out.println("Digite o número da conta: ");
+	int numeroConta = scanner.nextInt();
+	
+	System.out.println("Digite o saldo da conta: ");
+	double saldo = scanner.nextDouble();
+	
+	
+	System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nosso banco; sua agência é " + agencia + ", o número da sua conta: " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque!");
+}
+}
